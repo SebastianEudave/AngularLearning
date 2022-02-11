dfbecfaba492590cc553198bc5629dba9d2873c5
sebas <sebs.2901@gmail.com>
2022-02-11 08:00:46 -0600

    Added login functionality

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -9,9 +11,8 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
 
   model:any = {};
-  loggedIn: boolean = false;
 
-  constructor(private accountService: AccountService) { }
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
   }
@@ -20,7 +21,6 @@ export class NavComponent implements OnInit {
     this.accountService.login(this.model).subscribe(
       response => {
         console.log(response);
-        this.loggedIn = true;
       },
       error =>{
         console.log(error);
@@ -29,7 +29,7 @@ export class NavComponent implements OnInit {
   }
 
   logout(){
-    this.loggedIn = false;
+    this.accountService.logout();
   }
 
 }
